43234753707a3ccbf867f4fa8c7f2a320dd43454
octolink <paul@goocto.com>
2018-12-05 11:34:47 -0700

    ...

--- app/src/main/java/com/goocto/balltoy/Ball.kt
+++ app/src/main/java/com/goocto/balltoy/Ball.kt
@@ -4,7 +4,6 @@ import android.view.View
 import android.widget.ImageView
 import java.util.*
 
-//import com.goocto.balltoy.R.drawable.ball
 
 class Ball(private val ball: ImageView, private val area: View) {
 
